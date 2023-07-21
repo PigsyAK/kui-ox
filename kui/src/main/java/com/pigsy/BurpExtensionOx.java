5c0c29503a8eb9654cf3afb7f05240bea3c3fd00
G3G4X5X6 <87740076+G3G4X5X6@users.noreply.github.com>
2023-07-21 10:17:42 +0800

    add banner.txt

--- kui/src/main/java/com/pigsy/BurpExtensionOx.java
+++ kui/src/main/java/com/pigsy/BurpExtensionOx.java
@@ -3,28 +3,45 @@ package com.pigsy;
 import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
 import burp.api.montoya.logging.Logging;
+import burp.api.montoya.ui.menu.Menu;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class BurpExtensionOx implements BurpExtension {
     @Override
     public void initialize(MontoyaApi api) {
         // set extension name
         api.extension().setName("Kui-Ox");
+        showBanner(api.logging());
 
-        Logging logging = api.logging();
+        Menu menu = Menu.menu("Kui-Ox");
 
-        // write a message to our output stream
-        logging.logToOutput("Hello output.");
+        api.userInterface().menuBar().registerMenu(menu);
+        api.userInterface().registerSuiteTab("Kui-Ox", new JPanel());
+    }
 
-        // write a message to our error stream
-        logging.logToError("Hello error.");
+    private void showBanner(Logging logging) {
+        // 使用ClassLoader获取资源文件的输入流
+        InputStream inputStream = BurpExtensionOx.class.getClassLoader().getResourceAsStream("banner.txt");
 
-        // write a message to the Burp alerts tab
-        logging.raiseInfoEvent("Hello info event.");
-        logging.raiseDebugEvent("Hello debug event.");
-        logging.raiseErrorEvent("Hello error event.");
-        logging.raiseCriticalEvent("Hello critical event.");
-
-        // throw an exception that will appear in our error stream
-        throw new RuntimeException("Hello exception.");
+        StringBuilder banner = new StringBuilder();
+        if (inputStream != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            // 处理输入流，例如读取内容
+            try {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    banner.append(line).append("\n");
+                }
+            } catch (Exception e) {
+                logging.logToError(e.getMessage());
+            }
+        } else {
+            logging.logToError("资源文件未找到");
+        }
+        logging.logToOutput(banner.toString());
     }
 }
