c9eb60dfa3a699d784ae3b990babea7154ab28c7
G3G4X5X6 <87740076+G3G4X5X6@users.noreply.github.com>
2023-07-21 09:29:07 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Kui-Ox
+
 夔牛（Kui Ox），`Burpsuite` 插件，渗透小辅助
 
 ### 参考链接
+
 https://portswigger.net/burp/documentation/desktop/extensions/creating
 https://github.com/PortSwigger/burp-extensions-montoya-api
 https://github.com/PortSwigger/burp-extensions-montoya-api-examples
 
 ### TODO
+
 - [ ] 日志记录 `logging`
--- kui/src/main/java/com/pigsy/BurpExtensionOx.java
+++ kui/src/main/java/com/pigsy/BurpExtensionOx.java
@@ -2,10 +2,29 @@ package com.pigsy;
 
 import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.logging.Logging;
 
 public class BurpExtensionOx implements BurpExtension {
     @Override
     public void initialize(MontoyaApi api) {
+        // set extension name
+        api.extension().setName("Kui-Ox");
 
+        Logging logging = api.logging();
+
+        // write a message to our output stream
+        logging.logToOutput("Hello output.");
+
+        // write a message to our error stream
+        logging.logToError("Hello error.");
+
+        // write a message to the Burp alerts tab
+        logging.raiseInfoEvent("Hello info event.");
+        logging.raiseDebugEvent("Hello debug event.");
+        logging.raiseErrorEvent("Hello error event.");
+        logging.raiseCriticalEvent("Hello critical event.");
+
+        // throw an exception that will appear in our error stream
+        throw new RuntimeException("Hello exception.");
     }
 }
